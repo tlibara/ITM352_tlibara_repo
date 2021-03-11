d7e9475226b9dbf655ba356639f742a177fc6135
tlibara <77426986+tlibara@users.noreply.github.com>
2021-03-10 22:21:11 -1000

    Lab9 final

---
+++ Lab9/Ex1.js
@@ -0,0 +1,29 @@
+m = 9;
+d = 20;
+y = 2000;
+ys = String(y);
+
+step1 = Number(ys[2] + ys[3]);
+// 0
+step2 = step1 / 4;
+// 0
+step3 = step1 + step2;
+//0
+step4 = 5;
+step6 = step3 + step4;
+// 5
+step7 = step6 + d;
+// 25
+step8 = step7 - 1;
+// 24
+step8%7;
+// 3
+
+
+
+
+
+
+
+
+
