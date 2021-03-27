8d5d7ee4937a1a861e5f3b117a0ac2737f5543f4
tlibara <77426986+tlibara@users.noreply.github.com>
2021-03-27 00:14:49 -1000

    assignment

---
+++ Ibara_Toni-Lynn_Assignment1/products_data.js
@@ -0,0 +1,27 @@
+var products = [
+    {
+        'brand': "KYLIE Cosmetics",
+        'price': 42.00,
+        'image': "kylieshadow.png"
+    },
+    {
+        'brand': "James Charles X Morphe",
+        'price': 39.00,
+        'image': "jamesshadow.png"
+    },
+    {
+        'brand': "KKW Beauty",
+        'price': 45.00,
+        'image': "kkwshadow.png"
+    },
+    {
+        'brand': "Morphe X Manny MUA ",
+        'price': 30.00,
+        'image': "mannyshadow.png"
+    },
+    {
+        'brand': "Jeffree Star Cosmetics",
+        'price': 52.00,
+        'image': "jeffreeshadow.png"
+    }
+]
