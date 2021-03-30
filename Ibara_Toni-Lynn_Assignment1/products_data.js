90c2bf9475a802756aa872fde518724c31e4219b
tlibara <77426986+tlibara@users.noreply.github.com>
2021-03-29 15:18:40 -1000

    Assignment1

--- Ibara_Toni-Lynn_Assignment1/products_data.js
+++ Ibara_Toni-Lynn_Assignment1/products_data.js
@@ -2,26 +2,26 @@ var products = [
     {
         'brand': "KYLIE Cosmetics",
         'price': 42.00,
-        'image': "kylieshadow.png"
+        'image': "kylieshadow.jpg"
     },
     {
         'brand': "James Charles X Morphe",
         'price': 39.00,
-        'image': "jamesshadow.png"
+        'image': "jamesshadow.jpg"
     },
     {
         'brand': "KKW Beauty",
         'price': 45.00,
-        'image': "kkwshadow.png"
+        'image': "kkwshadow.jpg"
     },
     {
         'brand': "Morphe X Manny MUA ",
         'price': 30.00,
-        'image': "mannyshadow.png"
+        'image': "mannyshadow.jpg"
     },
     {
         'brand': "Jeffree Star Cosmetics",
         'price': 52.00,
-        'image': "jeffreeshadow.png"
+        'image': "jeffreeshadow.jpg"
     }
 ]
