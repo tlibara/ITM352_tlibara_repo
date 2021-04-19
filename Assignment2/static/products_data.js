12c440666dc6312dbb520ad81f8c8b8f80572f92
tlibara <77426986+tlibara@users.noreply.github.com>
2021-04-19 02:21:20 -1000

    assignment2

---
+++ Assignment2/static/products_data.js
@@ -0,0 +1,30 @@
+var products = [
+    {
+        'brand': "KYLIE Cosmetics",
+        'price': 42.00,
+        'image': "kylieshadow.jpg"
+    },
+    {
+        'brand': "James Charles X Morphe",
+        'price': 39.00,
+        'image': "jamesshadow.jpg"
+    },
+    {
+        'brand': "KKW Beauty",
+        'price': 45.00,
+        'image': "kkwshadow.jpg"
+    },
+    {
+        'brand': "Morphe X Manny MUA ",
+        'price': 30.00,
+        'image': "mannyshadow.jpg"
+    },
+    {
+        'brand': "Jeffree Star Cosmetics",
+        'price': 52.00,
+        'image': "jeffreeshadow.jpg"
+    }
+]
+if (typeof module != 'undefined') { 
+    module.exports.products = products;
+}
